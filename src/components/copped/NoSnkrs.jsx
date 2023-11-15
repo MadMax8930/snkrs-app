51943c4791278fbf5789b4ccd60b3c79848369a3
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-11-15 10:22:11 +0100

    notification logic done

---
+++ src/components/copped/NoSnkrs.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './copped.module.css';
+
+const NoSnkrs = () => {
+  return (
+    <div className={styles.nsContainer}>
+      <Link href="/" className={styles.nsLink}>
+         <h1 className={styles.nsTitle}>No sneakers found</h1>
+         <p className={styles.nsDescription}>You haven't copped any sneakers yet.</p>
+         <Image className={styles.nsImage} src="/snkrs.png" width={100} height={100} alt="No Copped Sneakers" />
+      </Link>
+    </div>
+  )
+}
+
+export default NoSnkrs
--- src/components/notif/Notification.jsx
+++ src/components/notif/Notification.jsx
@@ -1,56 +1,86 @@
 "use client";
 import axios from '../../../axios.config';
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import useNotificationsForSneaker from '@/hooks/useNotificationsForSneaker';
 import { toast } from 'react-hot-toast';
 import styles from './notification.module.css';
 
 const Notification = ({ sneakerId }) => {
    const { data: notifications, mutate: mutateNotifications } = useNotificationsForSneaker(sneakerId)
-
-   useEffect(() => {
-      console.log("user notif:", { notifications });
-    }, [notifications]);
+   const [loading, setLoading] = useState(false);
+   
+   const getNotificationId = (schedule) => {
+      const existingNotification = notifications?.find((notif) => notif.schedule === schedule);
+      return existingNotification ? existingNotification._id : null;
+   };
   
-   const scheduleAlreadySelectedForASneaker = (schedule) =>
-      notifications?.some((notif) => notif.schedule === schedule);
-  
-   const handleCheckboxChange = async (schedule) => {
+   const handleAddCheckbox = async (schedule) => {
       try {
+         setLoading(true);
          await axios.post('/profile/add-notification', { sneakerId, schedule }, { withCredentials: true });
          toast.success('Notification added successfully');
-         // Trigger a re-fetch of the data for the specific key
+         // Trigger a re-fetch data
          mutateNotifications();
       } catch (error) {
          console.error('Error adding notification:', error);
-         toast.error('Notification can not be added');
-      };
+         toast.error('Sorry! Please try again.');
+      } finally {
+         setLoading(false);
+      }
    };
 
+   const handleRemoveCheckbox = async (notificationId) => {
+      try {
+         setLoading(true);
+         await axios.delete(`/profile/notifications/${notificationId}`, { withCredentials: true });
+         toast.success('Notification removed successfully');
+         // Trigger a re-fetch data
+         mutateNotifications();
+      } catch (error) {
+         console.error('Error removing notification:', error);
+         toast.error('Sorry! Please try again.');
+      } finally {
+         setLoading(false);
+      }
+   };
+
+   const handleCheckboxToggle = async (schedule) => {
+      const notificationId = getNotificationId(schedule);
+
+      if (notificationId) {
+         // Checkbox is checked
+         await handleRemoveCheckbox(notificationId);
+      } else {
+         // Checkbox is unchecked
+         await handleAddCheckbox(schedule);
+      }
+   };
+   
+   if (loading) { return <p className='pt-2 lg:text-base md:text-sm text-xs text-gray-600'>Loading...</p>; }
+
   return (
-    <div className={styles.notification}>
-      Notification page:
+    <div className={styles.container}>
       <label>
         <input 
           type="checkbox" 
-          checked={scheduleAlreadySelectedForASneaker('1 day before')}
-          onChange={() => handleCheckboxChange('1 day before')}
+          checked={Boolean(getNotificationId('1 day before'))}
+          onChange={() => handleCheckboxToggle('1 day before')}
         />
         1 Day
       </label>
       <label>
         <input
           type="checkbox"
-          checked={scheduleAlreadySelectedForASneaker('3 days before')}
-          onChange={() => handleCheckboxChange('3 days before')}
+          checked={Boolean(getNotificationId('3 days before'))}
+          onChange={() => handleCheckboxToggle('3 days before')}
         />
         3 Days
       </label>
       <label>
         <input
           type="checkbox"
-          checked={scheduleAlreadySelectedForASneaker('1 week before')}
-          onChange={() => handleCheckboxChange('1 week before')}
+          checked={Boolean(getNotificationId('1 week before'))}
+          onChange={() => handleCheckboxToggle('1 week before')}
         />
         1 Week
       </label>
