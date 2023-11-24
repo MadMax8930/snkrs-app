500451b1b1c7b1d8d5515b28444c6f4d67f7f4bc
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-11-24 12:30:48 +0100

    mvp 5

--- axios.config.js
+++ axios.config.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.NEXT_PUBLIC_CONNECTION_TO_BACKEND || 'https://maxsneakers-backend.vercel.app';
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_CONNECTION_TO_BACKEND || 'http://localhost:3001';
 
 export default axios;
