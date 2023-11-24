90544d865af52d04ce9beacb18d371635a4cb1d0
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-11-24 22:09:12 +0100

    mvp 8.4

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -22,12 +22,12 @@ const setHeaderContentType = (req, res, next) => {
 };
 
 const customMiddleware = [
+   cors({ credentials: true, origin: process.env.EXPRESS_CONNECTION_TO_FRONTEND, optionsSuccessStatus: 204 }),
    express.json(),
-   cors({ credentials: true, origin: `${process.env.EXPRESS_CONNECTION_TO_FRONTEND}`, optionsSuccessStatus: 204 }),
    cookieParser(),
    logIncomingRequest, 
    logServerResponse, 
-   setHeaderContentType
+   setHeaderContentType,
 ];
 
 module.exports = { customMiddleware };
