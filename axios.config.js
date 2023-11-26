5d3141c8acc39cab061c65c5f664348e0dae34d8
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-11-26 12:30:21 +0100

    mvp 9

--- axios.config.js
+++ axios.config.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_CONNECTION_TO_BACKEND || 'http://localhost:3001';
+axios.defaults.adapter = ['xhr', 'http', 'https'];
 
 export default axios;
