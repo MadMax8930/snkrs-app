b9274061c6eaebb52fec995c233714152b8c7fcd
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-11-26 17:09:12 +0100

    cc

--- src/guards/withAuth.js
+++ src/guards/withAuth.js
@@ -11,18 +11,10 @@ export const withAuth = (WrappedComponent) => {
      const { setUser } = useContext(UserContext);
      const { data: profileData, isLoading: isLoadingProfile } = useUserProfile();
      const [cookies] = useCookies(['token']);
-     const router = useRouter();
-
+ 
      useEffect(() => {
        if (profileData) setUser(profileData);
      }, [profileData, setUser]);
-
-     useEffect(() => {
-       if (!cookies.token) {
-         const timer = setTimeout(() => router.push('/auth?variant=register'), 1500);
-         return () => clearTimeout(timer);
-       }
-     }, [cookies.token, router]);
 
      if (isLoadingProfile) return <LoaderLayer />;
 
